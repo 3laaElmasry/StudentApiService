d0499d5fd2edd7491063cb8d5b269f34662f7ded
Elmasry <3laaelmasry2005a@gmail.com>
2025-05-19 20:02:35 +0300

    Add Student Dto Model Validation

--- DataAccessLayer/DTO/StudentDto.cs
+++ DataAccessLayer/DTO/StudentDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,14 @@ namespace DataAccessLayer.DTO
     public class StudentDto
     {
         public int Id { get; set; }
+
+        [Required]
         public string? Name { get; set; }
+
+        [Range(15, 60)]
         public int Age { get; set; }
+
+        [Range(1, 100)]
         public int Grade { get; set; }
 
     }
