377a0df10f7b6b6f0d839591407dd4d22f2f09e6
Elmasry <3laaelmasry2005a@gmail.com>
2025-05-23 02:24:31 +0300

    Create Cities Controller With actoins in Frame Wordk Core

--- StudentApi/Controllers/CitiesController.cs
+++ StudentApi/Controllers/CitiesController.cs
@@ -1,7 +1,8 @@
-﻿using BuisnessLogicLayer.IServiceContracts;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using DataAccessLayer.Data;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using DataAccessLayer.Model;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace StudentApi.Controllers
 {
@@ -9,11 +10,94 @@ namespace StudentApi.Controllers
     [ApiController]
     public class CitiesController : ControllerBase
     {
-        private readonly ICityService _cityService;
+        private readonly ApplicationDbContext _context;
 
-        public CitiesController(ICityService cityService)
+        public CitiesController(ApplicationDbContext context)
         {
-            _cityService = cityService;
+            _context = context;
+        }
+
+        // GET: api/Cities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<City>>> GetCities()
+        {
+            return await _context.Cities.ToListAsync();
+        }
+
+        // GET: api/Cities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<City>> GetCity(Guid id)
+        {
+            var city = await _context.Cities.FindAsync(id);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return city;
+        }
+
+        // PUT: api/Cities/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCity(Guid id, City city)
+        {
+            if (id != city.CityId)
+            {
+                return BadRequest();
+            }
+
+            if (!CityExists(id))
+            {
+                return NotFound();
+            }
+            var cityFromDb = await _context.Cities.FirstOrDefaultAsync(x => x.CityId == id);
+
+            cityFromDb!.CityName = city.CityName;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cities
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<City>> PostCity(City city)
+        {
+            _context.Cities.Add(city);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCity", new { id = city.CityId }, city);
+        }
+
+        // DELETE: api/Cities/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCity(Guid id)
+        {
+            var city = await _context.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cities.Remove(city);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CityExists(Guid id)
+        {
+            return _context.Cities.Any(e => e.CityId == id);
         }
     }
 }
